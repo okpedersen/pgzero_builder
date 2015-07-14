75ce05a78dbd19cc1366537d4dddf1894b2a2187
Ole Kristian Pedersen <okpedersen@gmail.com>
2015-07-14 16:31:59 +0200

    Update install.sh - more testing needed

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 
 # Miniconda installation
 # Source: conda.pydata.org/docs/help/silent.html
-MINICONDA_OPTIONS= -b -p $HOME/miniconda
-MINICONDA_DOWNLOAD=""
+MINICONDA_OPTIONS=" -b -p $HOME/miniconda"
+MINICONDA_DOWNLOAD=''
 MINICONDA_LOCAL=""
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     MINICONDA_LOCAL="./Miniconda3-latest-Linux-x86_64.sh"
-    MINICONDA_DOWNLOAD="wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh"
+    MINICONDA_DOWNLOAD='/usr/bin/wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # TODO: OSX
     echo "OSX not implemented"
@@ -17,26 +18,28 @@ fi
 if [ ! -f $MINICONDA_LOCAL ]; then
     echo "Miniconda install script not found... Trying to download."
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        $MINICONDA_DOWLOAD
+        `$MINICONDA_DOWNLOAD`
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         # TODO: OSX / curl
         echo "OSX not implemented"
     fi
     if [ ! -f ./miniconda.sh ]; then
         echo "Download failed"
+	echo $MINICONDA_DOWNLOAD
         exit 1
     fi
-    bash ./miniconda.sh $MINICONDA_OPTIONS
+    bash ./miniconda.sh ${MINICONDA_OPTIONS}
 else
-    bash $MINICONDA_LOCAL $MINICONDA_OPTIONS
+    bash `$MINICONDA_LOCAL` ${MINICONDA_OPTIONS}
 fi
-export PATH="$HOME/miniconda/bin:$PATH"
+echo $'\n\nexport PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bashrc
 # End miniconda installation
 
 # Install pgzero
+CONDA_LOC=$HOME/miniconda/bin/conda
 if [ -f ./*pgzero*.tar.bz2 ]; then
-    conda install ./*pgzero*.tar.bz2
+    $CONDA_LOC install ./*pgzero*.tar.bz2
 else
-    conda install -c https://conda.binstar.org/okpedersen pgzero
+    $CONDA_LOC install -c https://conda.binstar.org/okpedersen pgzero
 fi
 # End install pgzero
